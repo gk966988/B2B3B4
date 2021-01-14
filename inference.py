d62d72615b842c9e8aa667747b19919a1219160e
gk966988 <44934894+gk966988@users.noreply.github.com>
2021-01-14 22:03:55 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -63,75 +63,12 @@ if __name__=='__main__':
     pred = torch.zeros(size=[len(imgs_name), 5])
     for config_file in config_files:
         output, imgs_name = load_model(config_file, imgs_path)
-        pred += output/3
+        pred += output.cpu()/3
     pred = F.softmax(pred, dim=1).cpu().numpy()
     pred = pred.argmax(1)
     sub = pd.DataFrame({'image_id': imgs_name, 'label': pred})
     # print(sub)
     sub.to_csv("submission.csv", index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# test_dataset = MyData(root=cfg.DATASETS.ROOT_TEST, phase='test',
-#                              transform=get_transform(cfg.INPUT.SIZE_TRAIN, 'test'))
-# test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False,
-#                              num_workers=2, pin_memory=True)
-
-
-
-
-# image_name = []
-# outputs = []
-#
-# num = 0
-# for config_file in config_files:
-#     num += 1
-#     preds = []
-#     cfg.merge_from_file(config_file)
-#     model = choose_net(name=cfg.MODEL.NAME, num_classes=cfg.MODEL.CLASSES, weight_path=cfg.MODEL.WEIGHT_FROM)
-#     weight_path = cfg.MODEL.MODEL_PATH + cfg.MODEL.NAME + '.pth'
-#     state_dict = torch.load(weight_path)
-#     model.load_state_dict(state_dict)
-#     print('Network loaded from {}'.format(weight_path))
-#     model.cuda()
-#     model.eval()
-#     with torch.no_grad():
-#         pbar = tqdm(enumerate(test_loader), total=int(len(test_loader.dataset)))
-#         pbar.set_description('Validation')
-#         for batch_idx, (img, _, img_name) in pbar:
-#             img = img.cuda()
-#             y_pred, y_metric  = model(img)
-#             # pred = F.softmax(y_pred, dim=1).cpu().numpy()
-#             pred = y_pred.cpu().numpy()
-#             preds.append(pred)
-#             output = np.concatenate(preds, 0)
-#             if num == len(config_files):
-#                 image_name.append(img_name[0])
-#     outputs.append(output)
-#
-# outputs = np.concatenate(outputs, 1)
-
-
-
-
-
-
-
-
-
 
 
 
